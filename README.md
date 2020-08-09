3c6efc9c7c0101d64b14fff7c33f1a7d025e0542
Víctor Donola Ferreira <victordonolaf@gmail.com>
2020-08-09 20:47:05 +0000

    translation of the code into English and changes to README.md

--- README.md
+++ README.md
@@ -1,24 +1,3 @@
-# jogo-genius
-<h3>Português</h3>
-
-Este programa é uma simulação de um jogo de memória eletrônico. 
-
-Eu não quis complicar muito o programa. O importante 
-é que já da pra brincar e ir aprendendo um pouco mais
-sobre Python e ncurses e que vocês entendam a lógica.
-
-Observação: 
-
-- Para rodar o programa use o Python3
-- No Debian, instale o pacote "sox" através do apt-get 
-para poder reproduzir os beeps através do "play"
-- O play é um utilitário de linha de comando (shell). Para
-testar o play direto do terminal copie e cole a linha abaixo: 
-
-play --no-show-progress --null -t alsa --channels 1 synth.5 sine 1000
-
-<h3>Inglês (English)</h3>
-
 This program is a simulation of an electronic memory game.
 
 I didn't want to complicate the program too much. The important
--- jogo-genius.py
+++ jogo-genius.py
@@ -8,18 +8,18 @@ import curses
 import random
 import os
 
-# janela principal
+# main window
 screen = curses.initscr()
 
-# tamanho da janela principal
+# main window size
 screen_x, screen_y = screen.getmaxyx()
 
 if screen_x < 24 or screen_y < 81:
 	screen.clear()
 	curses.endwin()
-	print('Tamanho atual do terminal: {} x {}'.format(screen_x, screen_y))
-	print('\nO seu terminal tem que ter no minimo 24 linhas por 81 colunas.\n')
-	print('Reajuste o tamanho do seu terminal para poder jogar... \n')
+	print('Current terminal size: {} x {}'.format(screen_x, screen_y))
+	print('\nYour terminal must have at least 24 lines per 81 columns.\n')
+	print('Readjust the size of your terminal to be able to play ... \n')
 	sys.exit(0)
 
 
@@ -27,52 +27,52 @@ screen.notimeout(False)
 screen.keypad(True)
 screen.clear()
 
-# atualiza a tela automaticamente mais causa perda de performance
-# pode ser usado no lugar das chamadas da funcao  screen.refresh()
-#screen.immedok(True)
+# updates the screen automatically but causes loss of performance
+# can be used in place of screen.refresh() function calls
+# screen.immedok(True)
 
-#screen.set_title('G E N I U S')
+# screen.set_title('G E N I U S')
 
 curses.mousemask(curses.ALL_MOUSE_EVENTS)
 
-# iniciando cores
+# starting colors
 curses.start_color()
 curses.use_default_colors()
 
-# Define as cores dos 'botãozões'
+# Defines 'buttonhole' colors
 curses.init_pair(1, curses.COLOR_WHITE, curses.COLOR_GREEN)
 curses.init_pair(2, curses.COLOR_WHITE, curses.COLOR_YELLOW)
 curses.init_pair(3, curses.COLOR_WHITE, curses.COLOR_RED)
 curses.init_pair(4, curses.COLOR_WHITE, curses.COLOR_BLUE)
 
-# Define cor do box lateral
+# Defines side box color
 curses.init_pair(10, curses.COLOR_YELLOW, curses.COLOR_BLACK)
 curses.init_pair(11, curses.COLOR_WHITE, curses.COLOR_BLACK)
 
-# Define cor dos textos
+# Sets text color
 curses.init_pair(12, curses.COLOR_BLUE, curses.COLOR_WHITE)
 
-# Define cor da Barra de Status
+# Sets color of the Status Bar
 curses.init_pair(13, curses.COLOR_WHITE, curses.COLOR_CYAN)
 
-# Define cor da tela de abertura
+# Sets the color of the splash screen
 curses.init_pair(14, curses.COLOR_GREEN, curses.COLOR_WHITE)
 
-# Define cor da mensagem de error
+# Defines error message color
 curses.init_pair(15, curses.COLOR_RED, curses.COLOR_WHITE)
 
 curses.cbreak()
 
-# Não retorna caracteres na tela
+# Does not return characters on the screen
 curses.noecho()
 
-# move o cursor para a posicao 0,0
-#screen.move(0,0)
+# moves the cursor to the 0,0 position
+# screen.move(0,0)
 
-# esconde o cursor do mouse e do terminal
+# hides the mouse and terminal cursor
 curses.curs_set(0)
 
-# Define Constantes
+# Defines Constants
 K_CTRL_Q = 17
 K_ESC    = 27
 K_1      = 49
@@ -80,14 +80,14 @@ K_2      = 50
 K_3      = 51
 K_4      = 52
 
-# Define Constantes usadas como caracteres especiais
+# Defines Constants used as special characters
 # Single-line
 # Chr( 218 ) + Chr( 196 ) + Chr( 191 ) ┌ ─ ┐
 # Chr( 179 ) + Chr(  32 ) + Chr( 179 ) │   │
 # Chr( 192 ) + Chr( 196 ) + Chr( 217 ) └ ─ ┘
 
-c_032 = chr(  32) # espaço
-c_177 = chr(9619) # bloco
+c_032 = chr(  32) # space
+c_177 = chr(9619) # block
 c_179 = chr(9474)
 c_191 = chr(9488)
 c_192 = chr(9492)
@@ -99,12 +99,12 @@ c_218 = chr(9484)
 class Box:
 
 	def Fill(lt,ce,lb,cd, cor):
-		#Desenha uma caixa sem bordas e sem sombras
+		#Draws a borderless and shadowless box
 		for x in range(lt,lb+1):
 			screen.addstr(x, ce, c_032 * (cd-ce+1), curses.color_pair(cor))
 
 	def Display(lt,ce,lb,cd,cor):
-		#Desenha uma caixa com bordas e sem sombras
+		#Draws a box with borders and no shadows
 		
 		__class__.Fill(lt,ce,lb,cd,cor)
 
@@ -121,11 +121,11 @@ class Box:
 			screen.addstr(x, cd, c_179, curses.color_pair(cor))
 
 	def Shadow(lt,ce,lb,cd,cor):
-		#Desenha uma caixa com bordas e com sombras
+		#Draws a box with borders and shadows
 		
 		__class__.Display(lt,ce,lb,cd,cor)
 
-		#Desenha a Sombra da Caixa
+		#Draw the Shadow of the Box
 		for x in range(lt+1,lb+1):
 			screen.addstr(x, cd+1, c_032, curses.color_pair(0))
 		
@@ -138,10 +138,10 @@ def Beep(duration = .1, frequency = 3000):
 	#os.system('beep -f %s -l %s' % (frequency,duration))
 	#os.system('beep -f 555 -l 460')
 
-	# Observação:
-	# O play é um utilitário de linha de comando (shell), para poder usa-lo
-	# instale o Pacote sox atraves do apt-get no Debian
-	# Para testar o play direto no bash copie e cole a linha abaixo:
+	# Note:
+    # Play is a command line utility (shell), so you can use it
+    # install the sox package via apt-get on Debian
+    # To test play directly in bash copy and paste the line below:
 	#play --no-show-progress --null -t alsa --channels 1 synth .5 sine 1000
 	try:
 		os.system('play --no-show-progress --null -t alsa --channels 1 synth %s sine %f' % (duration, frequency))
@@ -149,26 +149,26 @@ def Beep(duration = .1, frequency = 3000):
 		pass
 
 
-def pause(tempo):
-	# Atualiza a tela
+def pause(time):
+	# Updates the screen
 	screen.refresh()
 
-	# Pausa por um tempo
-	time.sleep(tempo)
+	# Pause for a while
+	time.sleep(time)
 
-	# Limpa qualquer outra coisa que o usuário tenha digitado
+	# Clears anything else the user has typed
 	curses.flushinp()
 
 
-def autor():
-	# Mostra a tela de abertura
-	# Obs: As 'fontes' usadas foram criadas através do programa
-	# figlet - http://www.figlet.org/
-	# digite o cmd abaixo para ver as fontes disponiveis
-	# showfigfonts
-	# digite o cmd abaixo para criar o arquivo com o texto
-	# figlet -f big "G e n i u s" >> genius.txt 
-	# figlet -f small "Versao 1.0" >> genius.txt 
+def author():
+	# Shows the opening screen
+    # Obs: The 'fonts' used were created through the program
+    # figlet - http://www.figlet.org/
+    # type the cmd below to see the available fonts
+    # showfigfonts
+    # type the cmd below to create the file with the text
+    # figlet -f big "G e n i u s" >> genius.txt
+    # figlet -f small "Version 1.0" >> genius.txt
 
 	Box.Shadow(1,10,20,68,12)
 
@@ -184,8 +184,8 @@ def autor():
 	screen.addstr(13, 20, ' \ V / -_) \'_(_-</ _` / _ \ | || () |', curses.color_pair(14) | curses.A_BOLD)
 	screen.addstr(14, 20, '  \_/\___|_| /__/\__,_\___/ |_(_)__/ ', curses.color_pair(14) | curses.A_BOLD)
                                      
-	screen.addstr(17, 15, 'Autor: Víctor Donola Ferreira (vdonoladev)', curses.color_pair(12))
-	screen.addstr(18, 15, 'Rua Antônio Marques, 64 - Triângulo', curses.color_pair(12))
+	screen.addstr(17, 15, 'author: Víctor Donola Ferreira (vdonoladev)', curses.color_pair(12))
+	screen.addstr(18, 15, 'Rua Divino, 64 - Centro', curses.color_pair(12))
 	screen.addstr(19, 28, 'Carangola - Minas Gerais', curses.color_pair(12))
 
 
@@ -197,12 +197,12 @@ def inkey():
 			key = screen.getch()
 
 			if key == curses.KEY_MOUSE:
-				# botao esquerdo do mouse foi pressionado
+				# left mouse button was pressed
 				_, mx, my, _, _ = curses.getmouse()
 				#screen.addstr (22, 1,"my = %i | mx = %i" % (my, mx))
 
-				# Verifica em que posicao foi pressionado
-				#        linhas            colunas 
+				# Check which position was pressed
+				#        lines            columns 
 				if   ( 4 <= my <= 10) and ( 4 <= mx <= 24):
 					return 1
 
@@ -220,13 +220,13 @@ def inkey():
 
 
 		if key in (K_ESC, K_CTRL_Q, ord('q'), ord('Q')):
-			# encerra o programa
-			screen.addstr(23, 1, 'Jogo abortado...       ', curses.color_pair(13) | curses.A_BOLD)
+			# closes the program
+			screen.addstr(23, 1, 'Game aborted ...       ', curses.color_pair(13) | curses.A_BOLD)
 			pause(3)
-			ENCERRA()
+			CLOSE()
 
 		elif key in (curses.KEY_F1, ord('h'), ord('H')):
-			# help acionado
+			# help triggered
 			return 0
 
 		elif key == K_1:
@@ -242,21 +242,21 @@ def inkey():
 			return 4
 
 
-def showBotao(n, cor=0):
-	#Desenha o botãozão
-	botao = {1:(4,4,10,24), 2:(4,26,10,46), 3:(12,4,18,24), 4:(12,26,18,46)}
+def showButton(n, cor=0):
+	#Draw the button
+	button = {1:(4,4,10,24), 2:(4,26,10,46), 3:(12,4,18,24), 4:(12,26,18,46)}
 	label = {1:(7,13), 2:(7,35), 3:(15,13), 4:(15,35)}
 	beep  = [500, 1100, 700, 900]
 
-	Box.Display(*botao[n], cor=cor)
+	Box.Display(*button[n], cor=cor)
 
 	screen.addstr(label[n][0], label[n][1], ' %s ' % n, curses.color_pair(12))
 
 	if cor: Beep(.12, beep[n-1])
 
 
-def display_tela():
-	#Cria um quadro na tela com char azul e fundo branco
+def screen_display():
+	#Creates a frame on the screen with blue char and white background
 	Box.Display(0,0,23,79,12)
 
 	Box.Display(1,50,22,77,11)
@@ -272,19 +272,19 @@ def display_tela():
 
 	screen.addstr(19, 52, 'Copyright (c) 2020-2021', curses.color_pair(10))
 
-	#Escreve o titulo
+	#Write the title
 	screen.addstr(2, 20, 'G E N I U S', curses.color_pair(12) | curses.A_BOLD)
 
-	#Desenha os botões
+	#Draw the buttons
 	for n in (1,2,3,4):
-		showBotao(n, n)
+		showButton(n, n)
 
-	#Limpa a Barra de Status
+	#Clear the Status Bar
 	screen.addstr(23, 0, c_032 * 80, curses.color_pair(13))
 	pause(.01)
 
-def ENCERRA():
-	#Restaura a cor do terminal
+def CLOSE():
+	#Restores the color of the terminal
 	screen.refresh()
 	screen.clear()
 	screen.keypad(False)
@@ -294,101 +294,101 @@ def ENCERRA():
 	sys.exit(0)
 
 
-def gerasequencia(size=10):
+def generatesequence(size=10):
 	return ''.join(random.choice('1234') for _ in range(size))
 
 def main():
-	msg   = {1:'Botão verde   ', 2:'Botão amarelo ', 3:'Botão vermelho', 4:'Botão azul    '}
+	msg   = {1:'Green button   ', 2:'Yellow button ', 3:'Red button', 4:'Blue button    '}
 
-	#Imprimi a tela de abertura
-	display_tela()
+	#Print the splash screen
+	screen_display()
 
-	autor()
+	author()
 
 	for _ in range(5):
-		# Fica piscando a tela (mudando de cor)
+		# The screen is flashing (changing color)
 		curses.init_pair(14, random.randint(0, 7), curses.COLOR_WHITE)
 		pause(.7)
 
 	niveis      = 32
-	seq_pc      = gerasequencia(niveis)
+	seq_pc      = generatesequence(niveis)
 	seq_user    = ''
 	dificuldade = 1
 
-	display_tela()
+	screen_display()
 
 	while True:
 
-		# mostra a sequencia gerada pelo computador (só pra depuração)
+		# shows the sequence generated by the computer (for debugging only)
 		#screen.addstr(21, 1, 'sequencia -> %s    ' % seq_pc[:dificuldade], curses.color_pair(12))
 
-		#Limpa a Barra de Status
+		#Clear the Status Bar
 		screen.addstr(23, 0, c_032 * 80, curses.color_pair(13))
 		screen.addstr(23, 61, 'Dificuldade: %5i ' % dificuldade, curses.color_pair(13) | curses.A_BOLD)
 		pause(1)
 
 		for n in range(dificuldade):
-			showBotao(int(seq_pc[n]), 0)
+			showButton(int(seq_pc[n]), 0)
 			pause(.2)
-			showBotao(int(seq_pc[n]), int(seq_pc[n]))
+			showButton(int(seq_pc[n]), int(seq_pc[n]))
 			pause(.2)
 
 
 		while True:
 
-			botao = inkey()
+			button = inkey()
 
-			if   botao == 0:
-				# help acionado
-				autor()
+			if   button == 0:
+				# help triggered
+				author()
 
-				screen.addstr(23, 1, 'Tecle algo para sair...', curses.color_pair(13) | curses.A_BOLD)
+				screen.addstr(23, 1, 'Hit something to leave ...', curses.color_pair(13) | curses.A_BOLD)
 
-				# O comando abaixo faz com que a chamada a getch() retorne depois
-				# de um tempo simulando a função inkey(<tempo>) do CLIPPER
+				# The command below causes the call to getch () to be returned later
+                # of a time simulating the CLIPPER inkey function (<time>)
 				curses.halfdelay(7)
 
 				while True:
 					key = screen.getch()
 					if key != -1:break
-					# Fica piscando a tela (mudando de cor)
+					# The screen is flashing (changing color)
 					curses.init_pair(14, random.randint(0, 7), curses.COLOR_WHITE)
 					screen.refresh()
 
-				display_tela()
+				screen_display()
 				#continue
 				break
 
-			elif 1 <= botao <= 4:
-				#Aciona o botãozão
-				screen.addstr(23, 1, msg[botao], curses.color_pair(13) | curses.A_BOLD)
-				showBotao(botao, 0)
+			elif 1 <= button <= 4:
+				#Click the button
+				screen.addstr(23, 1, msg[button], curses.color_pair(13) | curses.A_BOLD)
+				showButton(button, 0)
 				pause(.01)
-				showBotao(botao, botao)
+				showButton(button, button)
 				pause(.01)
 
-				seq_user += str(botao)
+				seq_user += str(button)
 
 				if seq_user == seq_pc:
 					Box.Shadow(9,23,15,55,12)
 					
-					screen.addstr(11, 25, 'Parabéns!!! Você conseguiu...', curses.color_pair(12))
-					screen.addstr(13, 25, 'Quer Jogar Novamente? (S/N) ', curses.color_pair(12))
+					screen.addstr(11, 25, 'Congratulations!!! You got it...', curses.color_pair(12))
+					screen.addstr(13, 25, 'Want to Play Again? (Y / N)', curses.color_pair(12))
 					pause(.01)
 					key = screen.getch()
 
-					if key in (ord('S'), ord('s')):
-						seq_pc      = gerasequencia(niveis)
+					if key in (ord('Y'), ord('y')):
+						seq_pc      = generatesequence(niveis)
 						seq_user    = ''
 						dificuldade = 1
-						display_tela()
+						screen_display()
 						break
 					else:
-						ENCERRA()
+						CLOSE()
 
 				elif seq_user == seq_pc[:len(seq_user)]:
 					if len(seq_user) == dificuldade:
-						screen.addstr(23, 16, 'OK  Próximo nivel...', curses.color_pair(13) | curses.A_BOLD)
+						screen.addstr(23, 16, 'OK  Next level...', curses.color_pair(13) | curses.A_BOLD)
 						pause(2)
 						seq_user     = ''
 						dificuldade += 1
@@ -405,23 +405,23 @@ def main():
 					screen.addstr(23, 16, 'NOK', curses.color_pair(13) | curses.A_BOLD)
 					Box.Shadow(9,23,15,55,15)
 					
-					screen.addstr(11, 26, 'Você   E R R O U ...', curses.color_pair(15) | curses.A_BLINK)
-					screen.addstr(13, 26, 'Quer Jogar Novamente? (S/N)', curses.color_pair(12))
+					screen.addstr(11, 26, 'You missed ...', curses.color_pair(15) | curses.A_BLINK)
+					screen.addstr(13, 26, 'Want to Play Again? (Y / N)', curses.color_pair(12))
 					pause(.01)
 					key = screen.getch()
 
-					if key in (ord('S'), ord('s')):
-						seq_pc      = gerasequencia(niveis)
+					if key in (ord('Y'), ord('y')):
+						seq_pc      = generatesequence(niveis)
 						seq_user    = ''
 						dificuldade = 1
-						display_tela()
+						screen_display()
 						break
 					else:
-						ENCERRA()
+						CLOSE()
 
 
 if __name__ == '__main__':
 	try:
 		curses.wrapper(main())
 	except KeyboardInterrupt:
-		ENCERRA()
+		CLOSE()
